c7e52df7371327895323f48e0e42623db3618b94
Facu Mediotte <facumediotte@gmail.com>
2021-08-23 12:39:32 -0300

    Dependency Injection @Mock, @InjectMocks and @ExtendWith

--- app-mockito/src/test/java/app/mockito/ejemplos/services/ExamenServiceImplTest.java
+++ app-mockito/src/test/java/app/mockito/ejemplos/services/ExamenServiceImplTest.java
@@ -5,6 +5,11 @@ import app.mockito.ejemplos.repositories.ExamenRepository;
 import app.mockito.ejemplos.repositories.PreguntaRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
 import java.util.List;
@@ -14,18 +19,27 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
+//Habilitamos el uso de annotations para esta clase
+@ExtendWith(MockitoExtension.class)
 class ExamenServiceImplTest {
 
+    @Mock
     private ExamenRepository examenRepository;
-    private ExamenService service;
+    @Mock
     private PreguntaRepository preguntaRepository;
+
+    @InjectMocks
+    private ExamenServiceImpl service;
 
     @BeforeEach
     void setUp() {
+        //Habilitamos el uso de annotations para esta clase
+        /*MockitoAnnotations.openMocks(this);
+
         //Simula una implementación de ExamenRepository
         examenRepository = mock(ExamenRepository.class);
         preguntaRepository = mock(PreguntaRepository.class);
-        service = new ExamenServiceImpl(examenRepository, preguntaRepository);
+        service = new ExamenServiceImpl(examenRepository, preguntaRepository);*/
     }
 
     @Test
