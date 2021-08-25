a67d585ce8035cb6e2495ef85a955ce4bc819f47
Facu Mediotte <facumediotte@gmail.com>
2021-08-24 23:29:05 -0300

    assertThrows and static method for class Datos to avoid conflict in the mock objects

--- springboot-test/src/main/java/test/springboot/app/Datos.java
+++ springboot-test/src/main/java/test/springboot/app/Datos.java
@@ -10,7 +10,19 @@ public class Datos {
     private Datos() {
     }
 
-    public static final Cuenta CUENTA_001 = new Cuenta(1L, "Facundo", new BigDecimal("1000"));
-    public static final Cuenta CUENTA_002 = new Cuenta(2L, "John", new BigDecimal("2000"));
-    public static final Banco BANCO = new Banco(1L, "El banco financiero", 0);
+//    public static final Cuenta CUENTA_001 = new Cuenta(1L, "Facundo", new BigDecimal("1000"));
+//    public static final Cuenta CUENTA_002 = new Cuenta(2L, "John", new BigDecimal("2000"));
+//    public static final Banco BANCO = new Banco(1L, "El banco financiero", 0);
+
+    public static Cuenta crearCuenta001() {
+        return new Cuenta(1L, "Facundo", new BigDecimal("1000"));
+    }
+
+    public static Cuenta crearCuenta002() {
+        return new Cuenta(2L, "John", new BigDecimal("2000"));
+    }
+
+    public static Banco crearBanco() {
+        return new Banco(1L, "El banco financiero", 0);
+    }
 }
--- springboot-test/src/test/java/test/springboot/app/SpringbootTestApplicationTests.java
+++ springboot-test/src/test/java/test/springboot/app/SpringbootTestApplicationTests.java
@@ -3,6 +3,7 @@ package test.springboot.app;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import test.springboot.app.exceptions.DineroInsuficienteException;
 import test.springboot.app.models.Banco;
 import test.springboot.app.models.Cuenta;
 import test.springboot.app.repositories.BancoRepository;
@@ -13,7 +14,9 @@ import test.springboot.app.services.CuentaServiceImpl;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
+import static test.springboot.app.Datos.*;
 
 @SpringBootTest
 class SpringbootTestApplicationTests {
@@ -27,13 +30,16 @@ class SpringbootTestApplicationTests {
         cuentaRepository = mock(CuentaRepository.class);
         bancoRepository = mock(BancoRepository.class);
         service = new CuentaServiceImpl(cuentaRepository, bancoRepository);
+//        Datos.CUENTA_001.setSaldo(new BigDecimal("1000"));
+//        Datos.CUENTA_002.setSaldo(new BigDecimal("2000"));
+//        Datos.BANCO.setTotalTransferencias(0);
     }
 
     @Test
     void contextLoads() {
-        when(cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
-        when(cuentaRepository.findById(2L)).thenReturn(Datos.CUENTA_002);
-        when(bancoRepository.findById(1L)).thenReturn(Datos.BANCO);
+        when(cuentaRepository.findById(1L)).thenReturn(crearCuenta001());
+        when(cuentaRepository.findById(2L)).thenReturn(crearCuenta002());
+        when(bancoRepository.findById(1L)).thenReturn(crearBanco());
 
         BigDecimal saldoOrigen = service.revisarSaldo(1L);
         BigDecimal saldoDestino = service.revisarSaldo(2L);
@@ -58,6 +64,39 @@ class SpringbootTestApplicationTests {
 
         verify(bancoRepository, times(2)).findById(1L);
         verify(bancoRepository).update(any(Banco.class));
+    }
+
+    @Test
+    void contextLoads2() {
+        when(cuentaRepository.findById(1L)).thenReturn(crearCuenta001());
+        when(cuentaRepository.findById(2L)).thenReturn(crearCuenta002());
+        when(bancoRepository.findById(1L)).thenReturn(crearBanco());
+
+        BigDecimal saldoOrigen = service.revisarSaldo(1L);
+        BigDecimal saldoDestino = service.revisarSaldo(2L);
+
+        assertEquals("1000", saldoOrigen.toPlainString());
+        assertEquals("2000", saldoDestino.toPlainString());
+
+        assertThrows(DineroInsuficienteException.class, () ->
+            service.transferir(1L, 2L, new BigDecimal("1200"), 1L)
+        );
+
+        saldoOrigen = service.revisarSaldo(1L);
+        saldoDestino = service.revisarSaldo(2L);
+
+        assertEquals("1000", saldoOrigen.toPlainString());
+        assertEquals("2000", saldoDestino.toPlainString());
+
+        int total = service.revisarTotalTransferencias(1L);
+        assertEquals(0, total);
+
+        verify(cuentaRepository, times(3)).findById(1L);
+        verify(cuentaRepository, times(2)).findById(2L);
+        verify(cuentaRepository, never()).update(any(Cuenta.class));
+
+        verify(bancoRepository).findById(1L);
+        verify(bancoRepository,never()).update(any(Banco.class));
     }
 
 }
