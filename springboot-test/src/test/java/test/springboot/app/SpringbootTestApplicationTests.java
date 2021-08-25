9e3f8970faecfaca69782344b2b4cbb1d49cd3cf
Facu Mediotte <facumediotte@gmail.com>
2021-08-24 23:37:28 -0300

    assertSame

--- springboot-test/src/test/java/test/springboot/app/SpringbootTestApplicationTests.java
+++ springboot-test/src/test/java/test/springboot/app/SpringbootTestApplicationTests.java
@@ -13,8 +13,7 @@ import test.springboot.app.services.CuentaServiceImpl;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static test.springboot.app.Datos.*;
 
@@ -64,6 +63,9 @@ class SpringbootTestApplicationTests {
 
         verify(bancoRepository, times(2)).findById(1L);
         verify(bancoRepository).update(any(Banco.class));
+
+        verify(cuentaRepository, times(6)).findById(anyLong());
+        verify(cuentaRepository, never()).findAll();
     }
 
     @Test
@@ -97,6 +99,23 @@ class SpringbootTestApplicationTests {
 
         verify(bancoRepository).findById(1L);
         verify(bancoRepository,never()).update(any(Banco.class));
+
+        verify(cuentaRepository, times(5)).findById(anyLong());
+        verify(cuentaRepository, never()).findAll();
     }
 
+    @Test
+    void contextLoads3() {
+        when(cuentaRepository.findById(1L)).thenReturn(crearCuenta001());
+
+        Cuenta cuenta1 = service.findById(1L);
+        Cuenta cuenta2 = service.findById(1L);
+
+        assertSame(cuenta1, cuenta2);
+        assertTrue(cuenta1 == cuenta2);
+        assertEquals("Facundo", cuenta1.getPersona());
+        assertEquals("Facundo", cuenta2.getPersona());
+
+        verify(cuentaRepository, times(2)).findById(1L);
+    }
 }
