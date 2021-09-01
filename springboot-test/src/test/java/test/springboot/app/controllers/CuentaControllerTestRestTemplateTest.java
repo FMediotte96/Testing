19136c0a8e70e445c1db3fafcf865bf7ea086063
Facu Mediotte <facumediotte@gmail.com>
2021-08-31 23:10:55 -0300

    IntegrationTest testTransferir with TestRestTemplate

--- springboot-test/src/test/java/test/springboot/app/controllers/CuentaControllerTestRestTemplateTest.java
+++ springboot-test/src/test/java/test/springboot/app/controllers/CuentaControllerTestRestTemplateTest.java
@@ -1,5 +1,7 @@
 package test.springboot.app.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +11,13 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import test.springboot.app.models.Cuenta;
 import test.springboot.app.models.TransactionDto;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -35,7 +41,7 @@ class CuentaControllerTestRestTemplateTest {
 
     @Test
     @Order(1)
-    void testTransferir() {
+    void testTransferir() throws JsonProcessingException {
         TransactionDto dto = new TransactionDto();
         dto.setMonto(new BigDecimal("100"));
         dto.setCuentaOrigenId(1L);
@@ -53,9 +59,41 @@ class CuentaControllerTestRestTemplateTest {
         assertNotNull(json);
         assertTrue(json.contains("Transferencia realizada con éxito!"));
         assertTrue(json.contains("{\"cuentaOrigenId\":1,\"cuentaDestinoId\":2,\"monto\":100,\"bancoId\":1},\"status\":\"OK\"}"));
+
+        JsonNode jsonNode = objectMapper.readTree(json);
+        assertEquals("Transferencia realizada con éxito!", jsonNode.path("mensaje").asText());
+        assertEquals(LocalDate.now().toString(), jsonNode.path("date").asText());
+        assertEquals("100", jsonNode.path("transaction").path("monto").asText());
+        assertEquals(1L, jsonNode.path("transaction").path("cuentaOrigenId").asLong());
+
+        Map<String, Object> response2 = new HashMap<>();
+        response2.put("date", LocalDate.now().toString());
+        response2.put("status", "OK");
+        response2.put("mensaje", "Transferencia realizada con éxito!");
+        response2.put("transaction", dto);
+
+        assertEquals(objectMapper.writeValueAsString(response2), json);
+    }
+
+    @Test
+    @Order(2)
+    void testDetalle() {
+        ResponseEntity<Cuenta> response = client.getForEntity(crearUri("/api/cuentas/1"), Cuenta.class);
+        Cuenta cuenta = response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+
+        assertNotNull(cuenta);
+        assertEquals(1L, cuenta.getId());
+        assertEquals("Facundo", cuenta.getPersona());
+        assertEquals("900.00", cuenta.getSaldo().toPlainString());
+        assertEquals(new Cuenta(1L, "Facundo", new BigDecimal("900.00")), cuenta);
     }
 
     private String crearUri(String uri) {
         return "http://localhost:" + port + uri;
     }
+
+
 }
